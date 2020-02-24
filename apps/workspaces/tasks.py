139b524b7a132ec6e237dde651424634c2a8ecf6
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2020-02-24 14:17:22 +0530

    Fixing infinite loop bug in Schedule

--- apps/workspaces/tasks.py
+++ apps/workspaces/tasks.py
@@ -13,7 +13,7 @@ def run_sync_schedule(workspace_id):
         workspace_id=workspace_id, state=['PAYMENT_PROCESSING'],
         export_non_reimbursable=False
     )
-    while task_log.status != 'COMPLETE':
+    while task_log.status != 'COMPLETE' and task_log.status != 'FATAL' and task_log.status != 'FAILED':
         task_log = TaskLog.objects.get(id=task_log.id)
         continue
     if task_log.status == 'COMPLETE':
