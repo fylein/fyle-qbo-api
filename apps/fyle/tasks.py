61c5cc83b144fd538d5ca2b1d93f9cc6d84bc619
Shekhar Chatterjee <shekhar.c@fyle.in>
2020-05-26 07:45:26 -0400

    Upload Fyle expense attachments to Quickbooks onlne

--- apps/fyle/tasks.py
+++ apps/fyle/tasks.py
@@ -83,7 +83,7 @@ def async_create_expense_groups(workspace_id: int, state: List[str], fund_source
 
             last_synced_at = workspace.last_synced_at
 
-            updated_at = ['gte:2019-10-09T07:57:37.063Z']
+            updated_at = []
 
             #if last_synced_at:
             #    updated_at.append('gte:{0}'.format(datetime.strftime(last_synced_at, '%Y-%m-%dT%H:%M:%S.000Z')))
