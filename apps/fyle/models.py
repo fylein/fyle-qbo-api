8b6f3ee575cb73a86891e7ed80029dc01d9bd89a
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2020-08-28 17:29:46 +0530

    Removed print statement

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -232,7 +232,6 @@ def _group_expenses(expenses, group_fields, workspace_id):
     for field in group_fields:
         if field.lower() not in ALLOWED_FIELDS:
             group_fields.pop(group_fields.index(field))
-            print(field.upper())
             field = ExpenseAttribute.objects.filter(workspace_id=workspace_id, attribute_type=field.upper()).first()
             custom_fields[field.attribute_type.lower()] = KeyTextTransform(field.display_name, 'custom_properties')
 
