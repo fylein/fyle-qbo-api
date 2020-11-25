00dfcb3cfaddbe0ea98bf2b5285e9f4a3e7ba532
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2020-11-26 01:15:51 +0530

    Auto import / Auto map Fyle Categories and QBO Accounts

--- apps/mappings/tasks.py
+++ apps/mappings/tasks.py
@@ -125,3 +125,111 @@ def schedule_projects_creation(import_projects, workspace_id):
 
         if schedule:
             schedule.delete()
+
+            
+def create_fyle_categories_payload(categories: List[DestinationAttribute]):
+    """
+    Create Fyle Categories Payload from QBO Customer / Categories
+    :param categories: QBO Categories
+    :return: Fyle Categories Payload
+    """
+    payload = []
+
+    for category in categories:
+        payload.append({
+            'name': category.value,
+            'code': category.destination_id,
+            'enabled': category.active
+        })
+
+    return payload
+
+
+def upload_categories_to_fyle(workspace_id):
+    """
+    Upload categories to Fyle
+    """
+    fyle_credentials: FyleCredential = FyleCredential.objects.get(workspace_id=workspace_id)
+    qbo_credentials: QBOCredential = QBOCredential.objects.get(workspace_id=workspace_id)
+
+    fyle_connection = FyleConnector(
+        refresh_token=fyle_credentials.refresh_token,
+        workspace_id=workspace_id
+    )
+
+    qbo_connection = QBOConnector(
+        credentials_object=qbo_credentials,
+        workspace_id=workspace_id
+    )
+
+    qbo_attributes: List[DestinationAttribute] = qbo_connection.sync_accounts(account_type='Expense')
+
+    fyle_payload: List[Dict] = create_fyle_categories_payload(qbo_attributes)
+
+    fyle_categories = fyle_connection.connection.Categories.post(fyle_payload)
+    fyle_connection.sync_categories(fyle_categories)
+    return qbo_attributes
+
+
+def auto_create_category_mappings(workspace_id):
+    """
+    Create Category Mappings
+    :return: mappings
+    """
+    MappingSetting.bulk_upsert_mapping_setting([{
+        'source_field': 'CATEGORY',
+        'destination_field': 'ACCOUNT'
+    }], workspace_id=workspace_id)
+
+    fyle_categories = upload_categories_to_fyle(workspace_id=workspace_id)
+
+    category_mappings = []
+
+    try:
+        for category in fyle_categories:
+            mapping = Mapping.create_or_update_mapping(
+                source_type='CATEGORY',
+                destination_type='ACCOUNT',
+                source_value=category.value,
+                destination_value=category.value,
+                workspace_id=workspace_id
+            )
+            category_mappings.append(mapping)
+
+        return category_mappings
+    except WrongParamsError as exception:
+        logger.exception(
+            'Error while creating categories workspace_id - %s in Fyle %s %s',
+            workspace_id, exception.message, {'error': exception.response}
+        )
+    except Exception:
+        error = traceback.format_exc()
+        error = {
+            'error': error
+        }
+        logger.exception(
+            'Error while creating categories workspace_id - %s error: %s',
+            workspace_id, error
+        )
+
+
+def schedule_categories_creation(import_categories, workspace_id):
+    if import_categories:
+        start_datetime = datetime.now()
+        schedule, _ = Schedule.objects.update_or_create(
+            func='apps.mappings.tasks.auto_create_category_mappings',
+            args='{}'.format(workspace_id),
+            defaults={
+                'schedule_type': Schedule.MINUTES,
+                'minutes': 24 * 60,
+                'next_run': start_datetime
+            }
+        )
+    else:
+        schedule: Schedule = Schedule.objects.filter(
+            func='apps.mappings.tasks.auto_create_category_mappings',
+            args='{}'.format(workspace_id)
+        ).first()
+
+        if schedule:
+            schedule.delete()
