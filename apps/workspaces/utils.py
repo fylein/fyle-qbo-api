d8223598c6bfbe2f82b34265ea6efd82d0209357
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2021-05-12 11:58:24 +0530

    Category versioning hack (#97)

--- apps/workspaces/utils.py
+++ apps/workspaces/utils.py
@@ -81,7 +81,11 @@ def create_or_update_general_settings(general_settings_payload: Dict, workspace_
     if workspace_general_settings:
         map_merchant_to_vendor = workspace_general_settings.map_merchant_to_vendor
 
-    category_sync_version = workspace_general_settings.category_sync_version if workspace_general_settings else 'v2'
+    # TODO: remove this hack once workspace settings are saved
+    if workspace_id == 98:
+        category_sync_version = 'v1'
+    else:
+        category_sync_version = workspace_general_settings.category_sync_version if workspace_general_settings else 'v2'
 
     general_settings, _ = WorkspaceGeneralSettings.objects.update_or_create(
         workspace_id=workspace_id,
