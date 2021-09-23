8689673aa0ae7a416eabc48f9dadf9639b862961
labhvam5 <labhvamkrsharma@gmail.com>
2021-09-23 17:37:04 +0530

    minor changes and adding cors-headers

--- apps/fyle/tasks.py
+++ apps/fyle/tasks.py
@@ -66,7 +66,7 @@ def async_create_expense_groups(workspace_id: int, fund_source: List[str], task_
         with transaction.atomic():
 
             workspace = Workspace.objects.get(pk=workspace_id)
-            
+
             last_synced_at = workspace.last_synced_at
 
             updated_at = []
