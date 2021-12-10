9fea56dadb4b4ab71395444e5de4749271437fe1
Nilesh Pant <nileshpant6969@gmail.com>
2021-12-10 15:18:29 +0530

    bug fix for syncing chart of account while onboarding

--- apps/quickbooks_online/utils.py
+++ apps/quickbooks_online/utils.py
@@ -125,7 +125,7 @@ class QBOConnector:
                 account['Name'] if category_sync_version == 'v1' else account['FullyQualifiedName']
             )
 
-            if account['AccountType'] in general_settings.charts_of_accounts and value:
+            if general_settings and account['AccountType'] in general_settings.charts_of_accounts and value:
                 account_attributes['account'].append({
                     'attribute_type': 'ACCOUNT',
                     'display_name': 'Account',
