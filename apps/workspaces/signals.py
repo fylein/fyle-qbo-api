6066524b51069cb6a975601e3bb8cb26657f8ba6
Sravan Kumar <sravan.kumar@fyle.in>
2021-12-16 12:43:09 +0530

    use post_save signal to sync_accounts

---
+++ apps/workspaces/signals.py
@@ -0,0 +1,24 @@
+"""
+Workspace Signals
+"""
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from apps.quickbooks_online.utils import QBOConnector
+from .models import WorkspaceGeneralSettings
+
+
+@receiver(post_save, sender=WorkspaceGeneralSettings)
+def run_post_configration_triggers(sender, instance: WorkspaceGeneralSettings, **kwargs):
+    """
+    :param sender: Sender Class
+    :param instance: Row Instance of Sender Class
+    :return: None
+    """
+    qbo_credentials: QBOCredential = QBOCredential.objects.get(workspace_id=workspace_id)
+
+    qbo_connection = QBOConnector(
+        credentials_object=qbo_credentials,
+        workspace_id=workspace_id
+    )
+    qbo_connection.sync_accounts()
