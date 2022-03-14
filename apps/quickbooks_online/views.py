c3c95b72ace55539023776b198fccb0de6bee9b8
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-03-14 17:07:44 +0530

    udpate

--- apps/quickbooks_online/views.py
+++ apps/quickbooks_online/views.py
@@ -343,38 +343,6 @@ class PreferencesView(generics.RetrieveAPIView):
             )
 
     def get(self, request, *args, **kwargs):
-        try:
-            qbo_credentials = QBOCredential.objects.get(workspace_id=kwargs['workspace_id'])
-
-            qbo_connector = QBOConnector(qbo_credentials, workspace_id=kwargs['workspace_id'])
-
-            preferences = qbo_connector.get_company_preference()
-
-            return Response(
-                data=preferences,
-                status=status.HTTP_200_OK
-            )
-        except QBOCredential.DoesNotExist:
-            return Response(
-                data={
-                    'message': 'QBO credentials not found in workspace'
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-        except WrongParamsError:
-            return Response(
-                data={
-                    'message': 'Quickbooks Online connection expired'
-                },
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-
-class CompanyInfoView(generics.RetrieveAPIView):
-    """
-    Preferences View
-    """
-    def get(self, request, *args, **kwargs):
         qbo_credentials = QBOCredential.objects.get(workspace_id=kwargs['workspace_id'])
         try:
             qbo_connector = QBOConnector(qbo_credentials, workspace_id=kwargs['workspace_id'])
@@ -395,6 +363,37 @@ class CompanyInfoView(generics.RetrieveAPIView):
         except WrongParamsError:
             qbo_credentials.refresh_token = None
             qbo_credentials.save()
+            return Response(
+                data={
+                    'message': 'Quickbooks Online connection expired'
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+
+class CompanyInfoView(generics.RetrieveAPIView):
+    """
+    Preferences View
+    """
+    def get(self, request, *args, **kwargs):
+        try:
+            qbo_credentials = QBOCredential.objects.get(workspace_id=kwargs['workspace_id'])
+            qbo_connector = QBOConnector(qbo_credentials, workspace_id=kwargs['workspace_id'])
+
+            company_info = qbo_connector.get_company_info()
+
+            return Response(
+                data=company_info,
+                status=status.HTTP_200_OK
+            )
+        except QBOCredential.DoesNotExist:
+            return Response(
+                data={
+                    'message': 'QBO credentials not found in workspace'
+                },
+                status=status.HTTP_400_BAD_REQUEST
+            )
+        except WrongParamsError:
             return Response(
                 data={
                     'message': 'Quickbooks Online connection expired'
