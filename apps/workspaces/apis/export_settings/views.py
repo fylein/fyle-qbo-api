60f7524a0c14a0983ec19abc220d7ee35f685717
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-03-23 13:56:45 +0530

    Reinstating Auth and Perms

--- apps/workspaces/apis/export_settings/views.py
+++ apps/workspaces/apis/export_settings/views.py
@@ -7,8 +7,6 @@ from .serializers import ExportSettingsSerializer
 
 class ExportSettingsView(generics.RetrieveUpdateAPIView):
     serializer_class = ExportSettingsSerializer
-    authentication_classes = []
-    permission_classes = []
 
     def get_object(self):
         return Workspace.objects.filter(id=self.kwargs['workspace_id']).first()
