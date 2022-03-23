b24168dbbdc7cfa7b2c890f738cc6537c38a3b27
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-03-23 09:22:17 +0530

    Store fyle_currency in the workspaces table (#201)
    Store fyle_currency in the workspaces table (#201)

--- apps/workspaces/models.py
+++ apps/workspaces/models.py
@@ -17,6 +17,7 @@ class Workspace(models.Model):
     name = models.CharField(max_length=255, help_text='Name of the workspace')
     user = models.ManyToManyField(User, help_text='Reference to users table')
     fyle_org_id = models.CharField(max_length=255, help_text='org id', unique=True)
+    fyle_currency = models.CharField(max_length=5, help_text='Fyle Currency', null=True)
     qbo_realm_id = models.CharField(max_length=255, help_text='qbo realm id')
     cluster_domain = models.CharField(max_length=255, help_text='fyle cluster domain', null=True)
     last_synced_at = models.DateTimeField(help_text='Datetime when expenses were pulled last', null=True)
