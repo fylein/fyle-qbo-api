385f2182c64a38d5a38e15a10414204e17f3095b
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-03-24 11:12:10 +0530

    Updating source based on destination

--- apps/workspaces/apis/import_settings/serializers.py
+++ apps/workspaces/apis/import_settings/serializers.py
@@ -9,7 +9,7 @@ from apps.mappings.models import GeneralMapping
 from apps.workspaces.models import Workspace, WorkspaceGeneralSettings
 
 
-class FilteredListSerializer(serializers.ListSerializer):
+class MappingSettingFilteredListSerializer(serializers.ListSerializer):
     """
     Serializer to filter the active system, which is a boolen field in
     System Model. The value argument to to_representation() method is
@@ -18,7 +18,7 @@ class FilteredListSerializer(serializers.ListSerializer):
 
     def to_representation(self, data):
         data = data.filter(destination_field__in=['CLASS', 'CUSTOMER', 'DEPARTMENT'])
-        return super(FilteredListSerializer, self).to_representation(data)
+        return super(MappingSettingFilteredListSerializer, self).to_representation(data)
 
 
 class ReadWriteSerializerMethodField(serializers.SerializerMethodField):
@@ -41,7 +41,7 @@ class ReadWriteSerializerMethodField(serializers.SerializerMethodField):
 class MappingSettingSerializer(serializers.ModelSerializer):
     class Meta:
         model = MappingSetting
-        list_serializer_class = FilteredListSerializer
+        list_serializer_class = MappingSettingFilteredListSerializer
         fields = ['source_field', 'destination_field', 'import_to_fyle', 'is_custom']
 
 
@@ -110,18 +110,27 @@ class ImportSettingsSerializer(serializers.Serializer):
                 'default_tax_code_id': general_mappings.get('default_tax_code').get('id')
             }
         )
+
+        if workspace_general_settings['import_tax_codes']:
+            mapping_settings.append({
+                'source_field': 'TAX_GROUP',
+                'destination_field': 'TAX_CODE',
+                'import_to_fyle': True,
+                'is_custom': False
+            })
+
         with transaction.atomic():
             for setting in mapping_settings:
-                mapping_setting, _ = MappingSetting.objects.update_or_create(
-                    source_field=setting['source_field'],
+                MappingSetting.objects.update_or_create(
+                    destination_field=setting['destination_field'],
                     workspace_id=instance.id,
                     defaults={
-                        'destination_field': setting['destination_field'],
+                        'source_field': setting['source_field'],
                         'import_to_fyle': setting['import_to_fyle'] if 'import_to_fyle' in setting else False,
                         'is_custom': setting['is_custom'] if 'is_custom' in setting else False
                     }
                 )
-                mapping_settings.append(mapping_setting)
+                # mapping_settings.append(mapping_setting)
 
         return instance
 
@@ -129,8 +138,8 @@ class ImportSettingsSerializer(serializers.Serializer):
         if not data.get('workspace_general_settings'):
             raise serializers.ValidationError('Workspace general settings are required')
 
-        if not data.get('expense_group_settings'):
-            raise serializers.ValidationError('Expense group settings are required')
+        if not data.get('mapping_settings'):
+            raise serializers.ValidationError('Mapping settings are required')
 
         if not data.get('general_mappings'):
             raise serializers.ValidationError('General mappings are required')
