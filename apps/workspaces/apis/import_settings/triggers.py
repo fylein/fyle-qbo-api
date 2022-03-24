add37b67fd7de990693440e9f20975ab3d9c8331
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-03-24 17:18:03 +0530

    Add Presave Triggers for saving Mappings Settings

--- apps/workspaces/apis/import_settings/triggers.py
+++ apps/workspaces/apis/import_settings/triggers.py
@@ -1,3 +1,6 @@
+from apps.mappings.tasks import schedule_cost_centers_creation, schedule_fyle_attributes_creation, schedule_projects_creation
+
+
 class ImportSettingsTrigger:
     """
     All the post save actions of Import Saettings API
@@ -13,8 +16,29 @@ class ImportSettingsTrigger:
         """
         pass
 
-    def post_save_mapping_settings(self):
+    def pre_save_mapping_settings(self):
         """
         Post save action for mapping settings
         """
-        pass
+        mapping_settings = self.__mapping_settings
+
+        projects_mapping_available = False
+        cost_center_mapping_available = False
+        custom_field_mapping_available = False
+
+        for setting in mapping_settings:
+            if setting['source_field'] == 'PROJECT':
+                projects_mapping_available = True
+            elif setting['source_field'] == 'COST_CENTER':
+                cost_center_mapping_available = True
+            elif setting['source_field'] == 'CUSTOM_FIELD':
+                custom_field_mapping_available = True
+        
+        if not projects_mapping_available:
+            schedule_projects_creation(False, self.__workspace_id)
+        
+        if not cost_center_mapping_available:
+            schedule_cost_centers_creation(False, self.__workspace_id)
+        
+        if not custom_field_mapping_available:
+            schedule_fyle_attributes_creation(self.__workspace_id)
