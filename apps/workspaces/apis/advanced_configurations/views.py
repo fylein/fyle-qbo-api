e0c777f0ec63357001a2e6241a5f4e5ec007005a
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-03-29 15:56:22 +0530

    removing extra tab

--- apps/workspaces/apis/advanced_configurations/views.py
+++ apps/workspaces/apis/advanced_configurations/views.py
@@ -7,6 +7,6 @@ from .serializers import AdvancedConfigurationsSerializer
 
 class AdvancedConfigurationsView(generics.RetrieveUpdateAPIView):
     serializer_class = AdvancedConfigurationsSerializer
-    
+
     def get_object(self):
         return Workspace.objects.filter(id=self.kwargs['workspace_id']).first()
