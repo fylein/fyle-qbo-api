01d85bb89e30dc4b14fc02977202b8dab858be94
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-03-30 19:07:54 +0530

    GET of Expense Groups along with expenses and date filter support

--- apps/fyle/serializers.py
+++ apps/fyle/serializers.py
@@ -4,13 +4,26 @@ from fyle_accounting_mappings.models import ExpenseAttribute
 from .models import Expense, ExpenseGroup, ExpenseGroupSettings, Reimbursement
 
 
+class ExpenseSerializer(serializers.ModelSerializer):
+    """
+    Expense serializer
+    """
+
+    class Meta:
+        model = Expense
+        fields = '__all__'
+
+
 class ExpenseGroupSerializer(serializers.ModelSerializer):
     """
     Expense group serializer
     """
+    expenses = ExpenseSerializer(many=True)
+
     class Meta:
         model = ExpenseGroup
         fields = '__all__'
+        extra_fields = ['expenses']
 
 
 class ExpenseGroupSettingsSerializer(serializers.ModelSerializer):
@@ -22,19 +35,11 @@ class ExpenseGroupSettingsSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 
-class ExpenseSerializer(serializers.ModelSerializer):
-    """
-    Expense serializer
-    """
-    class Meta:
-        model = Expense
-        fields = '__all__'
-
-
 class ExpenseFieldSerializer(serializers.ModelSerializer):
     """
     Expense Fields Serializer
     """
+
     class Meta:
         model = ExpenseAttribute
         fields = ['attribute_type', 'display_name']
@@ -44,6 +49,7 @@ class ReimbursementSerializer(serializers.ModelSerializer):
     """
     Reimbursement serializer
     """
+
     class Meta:
         model = Reimbursement
         fields = '__all__'
