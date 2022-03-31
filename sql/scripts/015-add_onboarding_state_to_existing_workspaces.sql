bdd964c39d52fb4cec9f0e178c7c7f0252719b24
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-03-31 19:14:49 +0530

    Update sql/scripts/015-add_onboarding_state_to_existing_workspaces.sql
    Co-authored-by: Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>

--- sql/scripts/015-add_onboarding_state_to_existing_workspaces.sql
+++ sql/scripts/015-add_onboarding_state_to_existing_workspaces.sql
@@ -82,7 +82,7 @@ from all_settings_view
 where 
     configuration_id is not null and general_mappings_id is not null and qbo_creds_id is null;
 
---- Update all of the above to have onboarding state set to 'MAP_EMPLOYEES'
+--- Update all of the above to have onboarding state set to 'COMPLETE'
 update workspaces 
 set 
     onboarding_state = 'COMPLETE' 
