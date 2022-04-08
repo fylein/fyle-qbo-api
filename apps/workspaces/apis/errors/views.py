ad24a2b4f5b55e1e8db688e2cda7529e15a30b41
Shwetabh Kumar <shwetabh.kumar@fyle.in>
2022-04-08 13:51:16 +0530

    fixing boolean filter

--- apps/workspaces/apis/errors/views.py
+++ apps/workspaces/apis/errors/views.py
@@ -12,10 +12,19 @@ class ErrorsView(generics.ListAPIView):
     
     def get_queryset(self):
         type = self.request.query_params.get('type')
+        
+        is_resolved = self.request.query_params.get('is_resolved')
+
+        if is_resolved.lower() == 'true':
+            is_resolved = True
+        else:
+            is_resolved = False
+
         params = {
-            'is_resolved': self.request.query_params.get('is_resolved', False),
+            'is_resolved': is_resolved,
             'workspace__id': self.kwargs.get('workspace_id')
         }
+
         if type:
             params['type'] = type
         
