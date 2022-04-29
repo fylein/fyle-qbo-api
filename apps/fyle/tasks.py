74d26ae0914cc412958d88a63fc128cbe01a7295
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-04-29 11:17:20 +0530

    sync expenses based on last_paid_at

--- apps/fyle/tasks.py
+++ apps/fyle/tasks.py
@@ -105,7 +105,8 @@ def async_create_expense_groups(workspace_id: int, fund_source: List[str], task_
                 state=expense_group_settings.expense_state,
                 last_synced_at=last_synced_at if expense_group_settings.expense_state == 'PAID' else None,
                 settled_at=last_synced_at if expense_group_settings.expense_state == 'PAYMENT_PROCESSING' else None,
-                filter_credit_expenses=filter_credit_expenses
+                filter_credit_expenses=filter_credit_expenses,
+                last_paid_at=last_synced_at if expense_group_settings.expense_state == 'PAID' else None
             )
 
             if expenses:
