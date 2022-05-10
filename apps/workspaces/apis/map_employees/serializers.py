7b8469dcd4a979cbd1a06df78c4f565d19b9dc59
Sravan Kumar <30795376+Sravanksk@users.noreply.github.com>
2022-05-10 16:58:08 +0530

    remove check for setting reimbursable_expenses_object as null on employee field mapping update

--- apps/workspaces/apis/map_employees/serializers.py
+++ apps/workspaces/apis/map_employees/serializers.py
@@ -33,9 +33,8 @@ class MapEmployeesSerializer(serializers.ModelSerializer):
         workspace_general_settings_instance = WorkspaceGeneralSettings.objects.filter(workspace_id=instance.id).first()
 
         if workspace_general_settings_instance and (workspace_general_settings_instance.employee_field_mapping != workspace_general_settings['employee_field_mapping']):
-            if workspace_general_settings_instance.reimbursable_expenses_object in ('BILL', 'CHECK'):
-                workspace_general_settings_instance.reimbursable_expenses_object = None
-                workspace_general_settings_instance.save()
+            workspace_general_settings_instance.reimbursable_expenses_object = None
+            workspace_general_settings_instance.save()
 
         workspace_general_settings_instance, _ = WorkspaceGeneralSettings.objects.update_or_create(
             workspace_id=workspace_id,
