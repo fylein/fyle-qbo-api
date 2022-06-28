f5a84bff1602b2a5572186f6cef0f6c57ca891a8
harshithaputtaswamy <harshi2680@gmail.com>
2022-06-28 22:10:53 +0530

    logger

--- apps/quickbooks_online/utils.py
+++ apps/quickbooks_online/utils.py
@@ -55,6 +55,7 @@ class QBOConnector:
         refresh_token = ''
 
         if 'QBO_TESTS_REFRESH_TOKENS' in os.environ:
+            print('QBO_TESTS_REFRESH_TOKENS - ', os.environ.get('QBO_TESTS_REFRESH_TOKENS'))
             refresh_tokens = ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS'))
             refresh_token = refresh_tokens[workspace_id]
         else:
