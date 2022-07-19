ddc9a87a9c73fbce85ceaf743989a7971fe8c6c9
harshithaputtaswamy <harshi2680@gmail.com>
2022-07-19 16:21:25 +0530

    assert statements and change few workskapce

--- tests/test_mappings/test_tasks.py
+++ tests/test_mappings/test_tasks.py
@@ -384,8 +384,8 @@ def test_auto_create_vendors_as_merchants(db, mocker):
     
     vendors = DestinationAttribute.objects.filter(workspace_id=workspace_id, attribute_type='VENDOR').count()
     expense_attribute = ExpenseAttribute.objects.filter(workspace_id=workspace_id, attribute_type='MERCHANT').count()
-    assert vendors == 30
-    assert expense_attribute == 30
+    assert vendors == 32
+    assert expense_attribute == 57
 
     fyle_credentials = FyleCredential.objects.get(workspace_id=1)
     fyle_credentials.delete()
