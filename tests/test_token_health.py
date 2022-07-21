87f6cd625f0515b0e094119f117df3d18118c29e
harshithaputtaswamy <harshi2680@gmail.com>
2022-07-21 14:37:52 +0530

    update action file to check token health

---
+++ tests/test_token_health.py
@@ -0,0 +1,28 @@
+
+import ast
+import os
+from apps.quickbooks_online.utils import QBOConnector, QBOCredential
+
+github_env_file = os.getenv('GITHUB_ENV')
+
+
+counter = 0
+
+refresh_tokens = ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS'))
+print(refresh_tokens)
+
+for workspace_id, refresh_token in refresh_tokens:
+    try:
+        qbo_credentials = QBOCredential.objects.get(workspace_id=workspace_id)
+        qbo_connection = QBOConnector(credentials_object=qbo_credentials, workspace_id=workspace_id)
+
+        print('qbo_connection succeded')
+
+
+    except Exception as error:
+        counter += 1
+        with open(github_env_file, "a") as env_file:
+            env_file.write("num_token_expired=" + counter)
+        print('error for workspace id - ', workspace_id)
+        print(error)
+        
