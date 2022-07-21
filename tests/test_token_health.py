971cc4ad3ee8c245a4f4ed7a565c5194f755cf27
harshithaputtaswamy <harshi2680@gmail.com>
2022-07-22 00:36:37 +0530

    minor change

--- tests/test_token_health.py
+++ tests/test_token_health.py
@@ -7,12 +7,7 @@ from apps.quickbooks_online.utils import QBOConnector, QBOCredential
 @pytest.mark.django_db
 def test_token_health():
     refresh_tokens = ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS'))
-    print(refresh_tokens)
-    print(os.environ)
-
-    counter = int(os.environ.get('NUM_TOKEN_EXPIRED'))
-    print(counter)
-    print(type(counter))
+    counter = 0
 
     for workspace_id in refresh_tokens.keys():
         try:
@@ -25,37 +20,6 @@ def test_token_health():
             counter += 1
             print('error for workspace id - ', workspace_id)
             print(error)
-
-    os.environ['NUM_TOKEN_EXPIRED'] = str(counter)
-    print("os.environ['NUM_TOKEN_EXPIRED']", os.environ['NUM_TOKEN_EXPIRED'])
-    with open('test_refresh_token_health.txt', 'w') as file:
-        file.write(str(counter))
-
-
-@pytest.mark.django_db
-def test_token_health_again():
-    refresh_tokens = ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS'))
-    print(refresh_tokens)
-    print(os.environ)
-
-    counter = int(os.environ.get('NUM_TOKEN_EXPIRED'))
-    print(counter)
-    print(type(counter))
-
-    for workspace_id in refresh_tokens.keys():
-        try:
-            qbo_credentials = QBOCredential.objects.get(workspace_id=workspace_id)
-            qbo_connection = QBOConnector(credentials_object=qbo_credentials, workspace_id=workspace_id)
-
-            print('qbo_connection succeded', qbo_connection)
-
-        except Exception as error:
-            counter += 1
-            print('error for workspace id - ', workspace_id)
-            print(error)
-
-    os.environ['NUM_TOKEN_EXPIRED'] = str(counter)
-    print("os.environ['NUM_TOKEN_EXPIRED']", os.environ['NUM_TOKEN_EXPIRED'])
 
     with open('test_refresh_token_health.txt', 'w') as file:
         file.write(str(counter))
