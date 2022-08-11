76e6d4ce5da84925ecd888efc3c25318bb35e5bc
labhvam5 <labhvam.s@fyle.in>
2022-08-11 13:52:14 +0530

    changes in reset_workspace sql function

--- sql/functions/reset-workspace.sql
+++ sql/functions/reset-workspace.sql
@@ -271,7 +271,7 @@ BEGIN
 --   RAISE NOTICE 'Deleted % workspaces', rcount;
 
     UPDATE workspaces
-    SET onboarding_state = 'CONNECTION', last_synced_at = null, destination_synced_at =  null, source_synced_at = null
+    SET onboarding_state = 'CONNECTION', last_synced_at = null, destination_synced_at =  null, source_synced_at = null, qbo_realm_id = null
     WHERE id = _workspace_id;
 
 RETURN;
