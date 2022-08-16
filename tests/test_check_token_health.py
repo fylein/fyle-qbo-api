26187622e79ee0c6a1095035d006ca706f396820
harshithaputtaswamy <44926484+harshithaputtaswamy@users.noreply.github.com>
2022-08-16 13:02:37 +0530

    Fix pytest to run step when previous step fails (#307)
    * fixed pytest
    
    * fixed pytest
    
    * fixed lint error

--- tests/test_check_token_health.py
+++ tests/test_check_token_health.py
@@ -10,22 +10,26 @@ logger = logging.getLogger(__name__)
 
 @pytest.mark.django_db
 def test_token_health():
-    refresh_tokens = ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS'))
     num_token_expired = 0
 
-    for workspace_id in refresh_tokens.keys():
-        try:
-            qbo_credentials = QBOCredential.objects.get(workspace_id=workspace_id)
-            qbo_connection = QBOConnector(credentials_object=qbo_credentials, workspace_id=workspace_id)
+    try:
+        refresh_tokens = ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS'))
+        for workspace_id in refresh_tokens.keys():
+            try:
+                qbo_credentials = QBOCredential.objects.get(workspace_id=workspace_id)
+                qbo_connection = QBOConnector(credentials_object=qbo_credentials, workspace_id=workspace_id)
 
-            logger.info('qbo_connection succeded - %s', qbo_connection)
+                logger.info('qbo_connection succeded - %s', qbo_connection)
 
-        except Exception as error:
-            num_token_expired += 1
-            logger.info('error for workspace id - %s', workspace_id)
-            logger.info('Error message - %s', error)
+            except Exception as error:
+                num_token_expired += 1
+                logger.info('error for workspace id - %s', workspace_id)
+                logger.info('Error message - %s', error)
 
-    logger.info('refresh tokens - %s', ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS')))
+        logger.info('refresh tokens - %s', ast.literal_eval(os.environ.get('QBO_TESTS_REFRESH_TOKENS')))
 
-    if num_token_expired != 0:
-        pytest.exit("Refresh token expired")
+        if num_token_expired != 0:
+            pytest.exit("Refresh token expired")
+
+    except Exception as error:
+        pytest.exit("Invalid Refresh token")
