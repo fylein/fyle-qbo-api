696ea05ec906c90ad42ab6d8ba900c35c9902988
harshithaputtaswamy <44926484+harshithaputtaswamy@users.noreply.github.com>
2022-11-07 16:20:29 +0530

    Added tests for previous bug reports (#352)
    * modified mappings and workspace tests
    
    * modified tests for fyle, mappings, workspace modules, 96.5% coverage
    
    * modified quickbooks module tests (#350)
    
    * modified quickbooks module tests
    
    * fixed comments
    
    * Add tests for prev bug reports
    
    * fixed comments

--- tests/test_workspaces/test_apis/test_import_settings/fixtures.py
+++ tests/test_workspaces/test_apis/test_import_settings/fixtures.py
@@ -66,10 +66,43 @@ data = {
     'response': {'workspace_general_settings':{'import_categories':True,'charts_of_accounts':['Expense'],'import_tax_codes':True,'import_vendors_as_merchants':True},'general_mappings':{'default_tax_code':{'name':'12.5% TR @12.5%','id':'22'}},'mapping_settings':[{'source_field':'COST_CENTER','destination_field':'CLASS','import_to_fyle':True,'is_custom':False,'source_placeholder':''},{'source_field':'PROJECT','destination_field':'DEPARTMENT','import_to_fyle':True,'is_custom':False,'source_placeholder':''},{'source_field':'CLASS','destination_field':'CUSTOMER','import_to_fyle':True,'is_custom':True,'source_placeholder':''}],
         'workspace_id':9
     },
-    'import_settings_missing_values': {
-        'mapping_settings': None,
-        'workspace_general_settings': {},
-        'general_mappings': {}
+    'invalid_general_mappings': {
+        'workspace_general_settings': {
+            'import_categories': True,
+            'charts_of_accounts': [
+                'Expense'
+            ],
+            'import_tax_codes': True,
+            'import_vendors_as_merchants': True
+        },
+        'general_mappings': {
+        },
+        'mapping_settings': [
+            {
+                'source_field': 'COST_CENTER',
+                'destination_field': 'DEPARTMENT',
+                'import_to_fyle': True,
+                'is_custom': False,
+                'source_placeholder': 'cost center'
+            }
+        ]
+    },
+    'invalid_mapping_settings': {
+        'workspace_general_settings': {
+            'import_categories': True,
+            'charts_of_accounts': [
+                'Expense'
+            ],
+            'import_tax_codes': True,
+            'import_vendors_as_merchants': True
+        },
+        'general_mappings': {
+            'default_tax_code': {
+                'name': '12.5% TR @12.5%',
+                'id': '22'
+            }
+        },
+        'mapping_settings': None
     },
 
 }
