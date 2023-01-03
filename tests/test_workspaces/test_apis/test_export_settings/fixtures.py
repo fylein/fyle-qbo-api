ae000e8339a31b3bac18feaa4b0437d7c3395d60
Vishal Pandey <vishalpandeyviptsk@gmail.com>
2023-01-03 11:31:43 +0530

    fixed tests for simplify_report_closure

--- tests/test_workspaces/test_apis/test_export_settings/fixtures.py
+++ tests/test_workspaces/test_apis/test_export_settings/fixtures.py
@@ -2,6 +2,7 @@ data = {
     'export_settings': {
         'expense_group_settings': {
             'expense_state': 'PAYMENT_PROCESSING',
+            'ccc_expense_state': 'PAID',
             'reimbursable_expense_group_fields': [],
             'reimbursable_export_date_type': '',
             'corporate_credit_card_expense_group_fields': [],
@@ -43,7 +44,7 @@ data = {
             'reimbursable_expenses_object':'EXPENSE','corporate_credit_card_expenses_object':'BILL'
         },
         'expense_group_settings': {
-            'reimbursable_expense_group_fields':['fund_source','claim_number','employee_email','report_id'],'corporate_credit_card_expense_group_fields':['fund_source','claim_number','employee_email','report_id'],'expense_state':'PAYMENT_PROCESSING','reimbursable_export_date_type':'current_date','ccc_export_date_type':'current_date'
+            'reimbursable_expense_group_fields':['fund_source','claim_number','employee_email','report_id'],'corporate_credit_card_expense_group_fields':['fund_source','claim_number','employee_email','report_id'],'expense_state':'PAYMENT_PROCESSING','ccc_expense_state':'PAID','reimbursable_export_date_type':'current_date','ccc_export_date_type':'current_date'
         },
         'general_mappings': {'accounts_payable':{'name':'Accumulated Depreciation','id':'3'},'qbo_expense_account':{'name':'Auto','id':'95'},'bank_account':{'name':'','id':''},'default_ccc_account':{'name':'','id':''},'default_debit_card_account':{'name':'','id':''},'default_ccc_vendor':{'name':'Arun','id':'87'}
         },
