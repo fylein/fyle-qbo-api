2ab2be4c350a024abd6540d6193aa474e32902d9
anishfyle <anishkumar.s@fyle.in>
2023-02-14 11:09:21 +0530

    serializers

--- apps/fyle/serializers.py
+++ apps/fyle/serializers.py
@@ -1,7 +1,7 @@
 from rest_framework import serializers
 
 from fyle_accounting_mappings.models import ExpenseAttribute
-from .models import Expense, ExpenseGroup, ExpenseGroupSettings, Reimbursement
+from .models import Expense, ExpenseGroup, ExpenseGroupSettings, ExpenseFilter, Reimbursement
 
 
 class ExpenseGroupSettingsSerializer(serializers.ModelSerializer):
@@ -12,15 +12,13 @@ class ExpenseGroupSettingsSerializer(serializers.ModelSerializer):
         model = ExpenseGroupSettings
         fields = '__all__'
 
-
 class ExpenseSerializer(serializers.ModelSerializer):
     """
     Expense serializer
     """
     class Meta:
         model = Expense
-        fields = '__all__'
-
+        fields = ['updated_at', 'claim_number', 'employee_email', 'employee_name', 'fund_source']
 
 class ExpenseGroupSerializer(serializers.ModelSerializer):
     """
@@ -32,6 +30,14 @@ class ExpenseGroupSerializer(serializers.ModelSerializer):
         model = ExpenseGroup
         fields = '__all__'
         extra_fields = ['expenses']
+
+class ExpenseGroupExpenseSerializer(serializers.ModelSerializer):
+    """
+    Expense Group Expense serializer
+    """
+    class Meta:
+        model = Expense
+        fields = '__all__'
 
 
 class ExpenseFieldSerializer(serializers.ModelSerializer):
@@ -50,3 +56,23 @@ class ReimbursementSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reimbursement
         fields = '__all__'
+
+class ExpenseFilterSerializer(serializers.ModelSerializer):
+    """
+    Expense Filter Serializer
+    """
+    class Meta:
+        model = ExpenseFilter
+        fields = '__all__'
+        read_only_fields = ('id', 'workspace', 'created_at', 'updated_at') 
+
+    def create(self, validated_data):
+        workspace_id = self.context['request'].parser_context.get('kwargs').get('workspace_id')
+
+        expense_filter, _ = ExpenseFilter.objects.update_or_create(
+            workspace_id=workspace_id,
+            rank=validated_data['rank'],
+            defaults=validated_data
+        )
+
+        return expense_filter
