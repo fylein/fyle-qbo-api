41e7e6e02ef2ade690afa0866eab22e62c7bc808
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-02-27 11:38:24 +0530

    Bump rest auth - loggers (#392)

--- apps/workspaces/tasks.py
+++ apps/workspaces/tasks.py
@@ -267,7 +267,7 @@ def run_email_notification(workspace_id):
         )
 
 def async_update_fyle_credentials(fyle_org_id: str, refresh_token: str):
-    fyle_credentials = FyleCredential.objects.get(workspace__fyle_org_id=fyle_org_id)
+    fyle_credentials = FyleCredential.objects.filter(workspace__fyle_org_id=fyle_org_id).first()
     if fyle_credentials:
         fyle_credentials.refresh_token = refresh_token
         fyle_credentials.save()
