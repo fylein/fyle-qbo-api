dcaf64ab3d516555143267a751d4dc78501a5963
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2023-03-21 17:11:16 +0530

    added expense_number and payment_number to ExpenseSerializer

--- apps/fyle/serializers.py
+++ apps/fyle/serializers.py
@@ -18,7 +18,7 @@ class ExpenseSerializer(serializers.ModelSerializer):
     """
     class Meta:
         model = Expense
-        fields = ['updated_at', 'claim_number', 'employee_email', 'employee_name', 'fund_source']
+        fields = ['updated_at', 'claim_number', 'employee_email', 'employee_name', 'fund_source', 'expense_number', 'payment_number']
 
 class ExpenseGroupSerializer(serializers.ModelSerializer):
     """
