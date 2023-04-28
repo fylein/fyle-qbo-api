20d2e55f2ffc0218d55af0ac276fb82472fad784
labhvam5 <labhvam.s@fyle.in>
2023-04-28 12:19:42 +0530

    adding test

--- tests/test_mappings/test_tasks.py
+++ tests/test_mappings/test_tasks.py
@@ -58,6 +58,70 @@ def test_auto_create_tax_codes_mappings(db, mocker):
     response = auto_create_tax_codes_mappings(workspace_id)
     assert response == None
 
+def test_disable_category_for_items_mapping(db ,mocker):
+    workspace_id = 5
+    workspace_general_setting = WorkspaceGeneralSettings.objects.filter(workspace_id=workspace_id).first()
+    workspace_general_setting.import_items = False
+    workspace_general_setting.save()
+
+    # mocking all the sdk calls
+    mocker.patch(
+        'fyle_integrations_platform_connector.apis.Categories.sync',
+        return_value=[]
+    )
+    mocker.patch(
+        'fyle_integrations_platform_connector.apis.Categories.post_bulk',
+        return_value=[]
+    )
+    mocker.patch(
+        'qbosdk.apis.Items.get',
+        return_value=[]
+    )
+
+    # adding test data to the database
+    destination_attribute = DestinationAttribute.objects.create(
+        attribute_type='ACCOUNT',
+        display_name='Item',
+        value='Concrete',
+        destination_id=3,
+        workspace_id=workspace_id,
+        active=False
+    )
+    expense_attribute = ExpenseAttribute.objects.create(
+        attribute_type='CATEGORY',
+        display_name='Category',
+        value='Concrete',
+        source_id='253737253737',
+        workspace_id=workspace_id,
+        active=True
+    )
+    Mapping.objects.create(
+        source_type='CATEGORY',
+        destination_type='ACCOUNT',
+        destination_id=destination_attribute.id,
+        source_id=expense_attribute.id,
+        workspace_id=workspace_id
+    )
+
+    disable_category_for_items_mapping(workspace_general_setting)
+
+    assert expense_attribute.active == False
+
+    with mock.patch('fyle_integrations_platform_connector.apis.Categories.sync') as mock_call:
+        mock_call.side_effect = WrongParamsError(msg='invalid params', response='invalid params')
+        disable_category_for_items_mapping(workspace_general_setting)
+
+        mock_call.side_effect = FyleInvalidTokenError(msg='Invalid Token for fyle', response='Invalid Token for fyle')
+        disable_category_for_items_mapping(workspace_general_setting)
+
+        mock_call.side_effect = Exception
+        disable_category_for_items_mapping(workspace_general_setting)
+
+    with mock.patch('qbosdk.apis.Items.get') as mock_call:
+        mock_call.side_effect = QBOCredential.DoesNotExist
+        disable_category_for_items_mapping(workspace_general_setting)
+
+
 def test_disable_category_for_items_mapping(db, mocker):
     workspace_id = 3
     mocker.patch(
