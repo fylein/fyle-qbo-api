f9f14e2c4b071b649f0df9b9e341bd74efc8316d
labhvam5 <labhvam.s@fyle.in>
2023-05-08 15:46:57 +0530

    fixing small bug

--- apps/workspaces/apis/export_settings/triggers.py
+++ apps/workspaces/apis/export_settings/triggers.py
@@ -5,19 +5,21 @@ from apps.workspaces.models import WorkspaceGeneralSettings
 
 class ExportSettingsTrigger:
     def __init__(self, workspace_general_settings: Dict, workspace_id: int):
-        self.workspace_general_settings = workspace_general_settings
-        self.workspace_id = workspace_id
+        self.__workspace_general_settings = workspace_general_settings
+        self.__workspace_id = workspace_id
 
-    def post_save_workspace_general_settings(self, workspace_general_settings_instance: WorkspaceGeneralSettings):
+    def post_save_workspace_general_settings(self):
         """
         Post save action for workspace general settings
         """
 
-        if (self.workspace_general_settings['reimbursable_expenses_object'] == 'JOURNAL ENTRY' or \
-            self.workspace_general_settings['corporate_credit_card_expenses_object'] == 'JOURNAL ENTRY') and\
-            workspace_general_settings_instance.import_items:
+        workspace_general_settings: WorkspaceGeneralSettings = WorkspaceGeneralSettings.objects.filter(workspace_id=self.__workspace_id).first()
+
+        if (self.__workspace_general_settings['reimbursable_expenses_object'] == 'JOURNAL ENTRY' or \
+            self.__workspace_general_settings['corporate_credit_card_expenses_object'] == 'JOURNAL ENTRY') and\
+            workspace_general_settings.import_items:
 
             # Disable category for items mapping and set import-items to flase
-            workspace_general_settings_instance.import_items = False
-            workspace_general_settings_instance.save()
-            async_task('apps.mappings.tasks.disable_category_for_items_mapping', workspace_general_settings_instance)
+            workspace_general_settings.import_items = False
+            workspace_general_settings.save()
+            async_task('apps.mappings.tasks.disable_category_for_items_mapping', workspace_general_settings)
