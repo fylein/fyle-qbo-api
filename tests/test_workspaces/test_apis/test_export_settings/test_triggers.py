015e4b4aba6ad0b059b8d5654d5f3ca1579c1aa0
labhvam5 <88420539+labhvam5@users.noreply.github.com>
2023-05-08 16:46:09 +0530

    Export items trigger bug fix  (#424)

--- tests/test_workspaces/test_apis/test_export_settings/test_triggers.py
+++ tests/test_workspaces/test_apis/test_export_settings/test_triggers.py
@@ -3,14 +3,20 @@ from apps.workspaces.apis.export_settings.triggers import ExportSettingsTrigger
 from apps.workspaces.models import WorkspaceGeneralSettings
 
 def test_post_save_workspace_general_settings_export_trigger(mocker, db):
+    # setting the import_items to True
     workspace_general_setting = WorkspaceGeneralSettings.objects.filter(workspace_id=5).first()
-    export_trigger = ExportSettingsTrigger(workspace_general_setting,5)
-
-    workspace_general_setting.reimbursable_expenses_object = 'JOURNAL ENTRY'
-    workspace_general_setting.corporate_credit_card_expenses_object = 'JOURNAL ENTRY'
     workspace_general_setting.import_items = True
     workspace_general_setting.save()
 
-    export_trigger.post_save_workspace_general_settings(workspace_general_setting)
+    # payload for the export trigger
+    workspace_general_settings_payload = {
+        'reimbursable_expenses_object': 'JOURNAL ENTRY',
+        'corporate_credit_card_expenses_object': 'JOURNAL ENTRY',
+    }
+    workspace_id = 5
 
+    export_trigger = ExportSettingsTrigger(workspace_general_settings=workspace_general_settings_payload, workspace_id=workspace_id)
+    export_trigger.post_save_workspace_general_settings()
+
+    workspace_general_setting = WorkspaceGeneralSettings.objects.filter(workspace_id=5).first()
     assert workspace_general_setting.import_items == False
