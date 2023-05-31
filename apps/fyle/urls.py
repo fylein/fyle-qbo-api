3545b94feef1d01e92a9d69ba99018448b46cf77
ruuushhh <rushikeshtayade2001@gmail.com>
2023-05-31 11:50:35 +0530

    Remove dead apis fyle app

--- apps/fyle/urls.py
+++ apps/fyle/urls.py
@@ -15,25 +15,17 @@ Including another URLconf
 """
 from django.urls import path
 
-from .views import ExpenseGroupView, ExpenseGroupByIdView, ExpenseGroupScheduleView, ExpenseView, EmployeeView, \
-    CategoryView, CostCenterView, ProjectView, ExpenseFieldsView, ExpenseCustomFieldsView, ExportableExpenseGroupsView, \
-    ExpenseGroupSettingsView, RefreshFyleDimensionView, SyncFyleDimensionView, ExpenseGroupCountView,\
-    ExpenseGroupSyncView, ExpenseFilterView, ExpenseGroupExpenseView, CustomFieldView
+from .views import ExpenseGroupView, ExpenseView, EmployeeView, \
+    ExpenseFieldsView, ExportableExpenseGroupsView, \
+    ExpenseGroupSettingsView, RefreshFyleDimensionView, SyncFyleDimensionView, \
+    ExpenseGroupSyncView, ExpenseFilterView, CustomFieldView
 
 urlpatterns = [
     path('expense_groups/', ExpenseGroupView.as_view(), name='expense-groups'),
-    path('expense_groups/count/', ExpenseGroupCountView.as_view(), name='expense-groups-count'),
     path('exportable_expense_groups/', ExportableExpenseGroupsView.as_view(), name='exportable-expense-groups'),
     path('expense_group_settings/', ExpenseGroupSettingsView.as_view(), name='expense-group-settings'),
-    path('expense_groups/trigger/', ExpenseGroupScheduleView.as_view()),
     path('expense_groups/sync/', ExpenseGroupSyncView.as_view(), name='sync-expense-groups'),
-    path('expense_groups/<int:expense_group_id>/', ExpenseGroupByIdView.as_view(), name='expense-group-by-id'),
-    path('expense_groups/<int:expense_group_id>/expenses/', ExpenseGroupExpenseView.as_view(), name='expense-group-by-id-expenses'),
     path('employees/', EmployeeView.as_view(), name='employees'),
-    path('categories/', CategoryView.as_view(), name='categories'),
-    path('cost_centers/', CostCenterView.as_view(), name='cost-centers'),
-    path('projects/', ProjectView.as_view(), name='projects'),
-    path('expense_custom_fields/', ExpenseCustomFieldsView.as_view()),
     path('expense_fields/', ExpenseFieldsView.as_view(), name='expense-fields'),
     path('sync_dimensions/', SyncFyleDimensionView.as_view(), name='sync-fyle-dimensions'),
     path('refresh_dimensions/', RefreshFyleDimensionView.as_view(), name='refresh-fyle-dimensions'),
