3f6fa400097c9b71cb6af81797a618dbef68d6ce
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2023-06-01 09:28:14 +0530

    Remove dead apis for tasks users (#437)
    * Remove dead apis for tasks users
    
    * comments resolve

--- tests/test_tasks/test_views.py
+++ tests/test_tasks/test_views.py
@@ -25,14 +25,4 @@ def test_get_task_by_id(api_client, test_connection):
         'id': '8'
     })
     assert response.status_code==200
-
-
-def test_get_task_by_expense_group_id(api_client, test_connection):
-    access_token = test_connection.access_token
-    url = '/api/workspaces/3/tasks/expense_group/9/'
-
-    api_client.credentials(HTTP_AUTHORIZATION='Bearer {}'.format(access_token))
-
-    response = api_client.get(url)
-    assert response.status_code==200
     
