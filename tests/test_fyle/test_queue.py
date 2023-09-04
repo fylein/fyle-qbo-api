4db40b41138451b91bfb5b7565995687019461a4
ashwin1111 <winash1998@gmail.com>
2023-09-04 16:50:10 +0530

    fix lint

--- tests/test_fyle/test_queue.py
+++ tests/test_fyle/test_queue.py
@@ -1,5 +1,6 @@
 from apps.fyle.queue import async_post_accounting_export_summary
 
+
 # This test is just for cov :D
 def test_async_post_accounting_export_summary(db):
     async_post_accounting_export_summary(1, 1)
