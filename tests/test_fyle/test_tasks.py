727cffa1b227e5d4c09ba04c6b2331ef8fc7f652
ashwin1111 <winash1998@gmail.com>
2023-09-04 16:40:21 +0530

    add tests

--- tests/test_fyle/test_tasks.py
+++ tests/test_fyle/test_tasks.py
@@ -1,13 +1,17 @@
 import json
 from unittest import mock
 
+from django.conf import settings
+from django.db.models import Q
+
 import pytest
 from django.urls import reverse
 
 from apps.fyle.models import Expense, ExpenseGroup, ExpenseGroupSettings
-from apps.fyle.tasks import create_expense_groups
+from apps.fyle.tasks import create_expense_groups, __get_updated_accounting_export_summary, \
+    __mark_accounting_export_summary_as_synced, __mark_expenses_as_skipped, post_accounting_export_summary
 from apps.tasks.models import TaskLog
-from apps.workspaces.models import FyleCredential
+from apps.workspaces.models import FyleCredential, Workspace
 from tests.helper import dict_compare_keys
 from tests.test_fyle.fixtures import data
 
@@ -80,3 +84,70 @@ def test_create_expense_group_skipped_flow(mocker, api_client, test_connection):
         for expense in expenses:
             if expense.employee_email == 'jhonsnow@fyle.in':
                 assert expense.is_skipped == True
+
+def test_get_updated_accounting_export_summary():
+    updated_accounting_export_summary = __get_updated_accounting_export_summary('tx_123', True)
+    expected_updated_accounting_export_summary = {
+        'id': 'tx_123',
+        'state': 'SKIPPED',
+        'error_type': None,
+        'url': '{}/workspaces/main/export_log'.format(settings.QBO_INTEGRATION_APP_URL),
+        'synced': True
+    }
+
+    assert updated_accounting_export_summary == expected_updated_accounting_export_summary
+
+    updated_accounting_export_summary = __get_updated_accounting_export_summary('tx_123', False)
+    expected_updated_accounting_export_summary = {
+        'id': 'tx_123',
+        'state': 'SKIPPED',
+        'error_type': None,
+        'url': '{}/workspaces/main/export_log'.format(settings.QBO_INTEGRATION_APP_URL),
+        'synced': False
+    }
+
+    assert updated_accounting_export_summary == expected_updated_accounting_export_summary
+
+def test_mark_accounting_export_summary_as_synced(db):
+    expenses = Expense.objects.filter(org_id='or79Cob97KSh')
+    for expense in expenses:
+        expense.accounting_export_summary = __get_updated_accounting_export_summary(expense.expense_id, True)
+        expense.save()
+
+    expenses = Expense.objects.filter(org_id='or79Cob97KSh')
+    
+    __mark_accounting_export_summary_as_synced(expenses)
+
+    expenses = Expense.objects.filter(org_id='or79Cob97KSh')
+
+    for expense in expenses:
+        assert expense.accounting_export_summary['synced'] == True
+
+def test_mark_expenses_as_skipped(db):
+    expense_group = ExpenseGroup.objects.filter(workspace_id=3).first()
+    expense_id = expense_group.expenses.first().id
+    expense_group.expenses.remove(expense_id)
+
+    workspace = Workspace.objects.get(id=3)
+    __mark_expenses_as_skipped(Q(), [expense_id], workspace)
+
+    expense = Expense.objects.filter(id=expense_id).first()
+
+    assert expense.is_skipped == True
+    assert expense.accounting_export_summary['synced'] == False
+
+
+def test_post_accounting_export_summary(db, mocker):
+    expense_group = ExpenseGroup.objects.filter(workspace_id=3).first()
+    expense_id = expense_group.expenses.first().id
+    expense_group.expenses.remove(expense_id)
+
+    workspace = Workspace.objects.get(id=3)
+    __mark_expenses_as_skipped(Q(), [expense_id], workspace)
+
+    assert Expense.objects.filter(id=expense_id).first().accounting_export_summary['synced'] == False
+
+    mocker.patch('fyle_integrations_platform_connector.apis.Expenses.post_bulk_accounting_export_summary', return_value=data['expenses'])
+    post_accounting_export_summary('or79Cob97KSh', 3)
+
+    assert Expense.objects.filter(id=expense_id).first().accounting_export_summary['synced'] == True
