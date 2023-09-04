b8d7d316f35f65ed853bec6e9caa9de1fe5bfee4
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-09-04 14:31:28 +0530

    Update Integration status on disconnection (#475)
    * Update Integration status on disconnection
    
    * add test and fix lint

--- apps/workspaces/apis/advanced_configurations/triggers.py
+++ apps/workspaces/apis/advanced_configurations/triggers.py
@@ -1,13 +1,9 @@
 import logging
-import json
-from datetime import datetime
 
-from django.conf import settings
-
-from apps.fyle.helpers import post_request
 from apps.mappings.queue import schedule_bill_payment_creation
 from apps.quickbooks_online.queue import schedule_qbo_objects_status_sync, schedule_reimbursements_sync
-from apps.workspaces.models import WorkspaceGeneralSettings, FyleCredential
+from apps.workspaces.models import WorkspaceGeneralSettings
+from apps.workspaces.actions import post_to_integration_settings
 
 logger = logging.getLogger(__name__)
 logger.level = logging.INFO
@@ -30,21 +26,8 @@ class AdvancedConfigurationsTriggers:
         schedule_reimbursements_sync(sync_qbo_to_fyle_payments=workspace_general_settings_instance.sync_qbo_to_fyle_payments, workspace_id=workspace_general_settings_instance.workspace.id)
 
     @staticmethod
-    def post_to_integration_settings(workspace_id: int):
+    def post_to_integration_settings(workspace_id: int, active: bool):
         """
         Post to integration settings
         """
-        refresh_token = FyleCredential.objects.get(workspace_id=workspace_id).refresh_token
-        url = '{}/integrations/'.format(settings.INTEGRATIONS_SETTINGS_API)
-        payload = {
-            'tpa_id': settings.FYLE_CLIENT_ID,
-            'tpa_name': 'Fyle Quickbooks Integration',
-            'type': 'ACCOUNTING',
-            'is_active': True,
-            'connected_at': datetime.now().strftime('%Y-%m-%dT%H:%M:%S.%fZ')
-        }
-
-        try:
-            post_request(url, json.dumps(payload), refresh_token)
-        except Exception as error:
-            logger.error(error)
+        post_to_integration_settings(workspace_id, active)
