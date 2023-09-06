19a617237cb56c532494cc542d9b8b4e8eafe718
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-09-06 12:08:39 +0530

    Add workspace_id to Expenses and fill data (#480)
    * Add workspace_id to Expenses and fill data
    
    * update fixture and lint
    
    * Update filename

--- apps/quickbooks_online/helpers.py
+++ apps/quickbooks_online/helpers.py
@@ -2,25 +2,36 @@ from apps.fyle.models import ExpenseGroup
 
 
 def generate_export_type_and_id(expense_group: ExpenseGroup) -> tuple:
+    """
+    Generate export type and id
+    :param expense_group: Expense group object
+    """
     export_type = None
     export_id = None
 
+    # If Bill exists, export_type is bill
     if 'Bill' in expense_group.response_logs and expense_group.response_logs['Bill']:
         export_type = 'bill'
         export_id = expense_group.response_logs['Bill']['Id']
+    # If JournalEntry exists, export_type is journal
     elif 'JournalEntry' in expense_group.response_logs and expense_group.response_logs['JournalEntry']:
         export_type = 'journal'
         export_id = expense_group.response_logs['JournalEntry']['Id']
+    # If Purchase exists, export_type can be check / expense
     elif 'Purchase' in expense_group.response_logs and expense_group.response_logs['Purchase']:
         export_id = expense_group.response_logs['Purchase']['Id']
+        # If PaymentType is Check, export_type is check
         if expense_group.response_logs['Purchase']['PaymentType'] == 'Check':
             export_type = 'check'
         else:
+            # Defaulting to expense initially
             export_type = 'expense'
+            # If PaymentType is CreditCard, export_type is creditcardcredit
             if expense_group.fund_source == 'CCC' and \
                 expense_group.response_logs['Purchase']['PaymentType'] == 'CreditCard' and \
                     expense_group.response_logs['Purchase'].get('Credit'):
                 export_type = 'creditcardcredit'
+            # If PaymentType is Cash, export_type is expense
             elif expense_group.fund_source == 'CCC' \
                 and expense_group.response_logs['Purchase']['PaymentType'] == 'Cash':
                 export_type = 'expense'
