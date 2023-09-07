d308dd8e651be01c80c9bec3707de5aa606d52cb
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-09-07 19:01:52 +0530

    Handle empty failed updates for auto runs (#485)

--- apps/workspaces/tasks.py
+++ apps/workspaces/tasks.py
@@ -113,6 +113,7 @@ def export_to_qbo(workspace_id, export_mode=None):
     last_export_detail = LastExportDetail.objects.get(workspace_id=workspace_id)
     last_exported_at = datetime.now()
     is_expenses_exported = False
+    export_mode = export_mode or 'MANUAL'
 
     if general_settings.reimbursable_expenses_object:
 
@@ -122,16 +123,32 @@ def export_to_qbo(workspace_id, export_mode=None):
             is_expenses_exported = True
 
         if general_settings.reimbursable_expenses_object == 'BILL':
-            schedule_bills_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_bills_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
 
         elif general_settings.reimbursable_expenses_object == 'EXPENSE':
-            schedule_qbo_expense_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_qbo_expense_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
 
         elif general_settings.reimbursable_expenses_object == 'CHECK':
-            schedule_cheques_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_cheques_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
 
         elif general_settings.reimbursable_expenses_object == 'JOURNAL ENTRY':
-            schedule_journal_entry_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_journal_entry_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
 
     if general_settings.corporate_credit_card_expenses_object:
         expense_group_ids = ExpenseGroup.objects.filter(fund_source='CCC', exported_at__isnull=True, workspace_id=workspace_id).values_list('id', flat=True)
@@ -140,19 +157,35 @@ def export_to_qbo(workspace_id, export_mode=None):
             is_expenses_exported = True
 
         if general_settings.corporate_credit_card_expenses_object == 'JOURNAL ENTRY':
-            schedule_journal_entry_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_journal_entry_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
 
         elif general_settings.corporate_credit_card_expenses_object == 'CREDIT CARD PURCHASE':
-            schedule_credit_card_purchase_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_credit_card_purchase_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
 
         elif general_settings.corporate_credit_card_expenses_object == 'DEBIT CARD EXPENSE':
-            schedule_qbo_expense_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_qbo_expense_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
 
         elif general_settings.corporate_credit_card_expenses_object == 'BILL':
-            schedule_bills_creation(workspace_id=workspace_id, expense_group_ids=expense_group_ids)
+            schedule_bills_creation(
+                workspace_id=workspace_id,
+                expense_group_ids=expense_group_ids,
+                is_auto_export=export_mode == 'AUTO'
+            )
     if is_expenses_exported:
         last_export_detail.last_exported_at = last_exported_at
-        last_export_detail.export_mode = export_mode or 'MANUAL'
+        last_export_detail.export_mode = export_mode
         last_export_detail.save()
 
 
