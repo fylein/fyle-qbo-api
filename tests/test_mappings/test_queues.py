52bf72ac68f3993bff1051136d267e1e3ea5327a
labhvam5 <labhvam.s@fyle.in>
2023-11-22 19:36:03 +0530

    adding new test

---
+++ tests/test_mappings/test_queues.py
@@ -0,0 +1,15 @@
+from apps.mappings.queues import construct_tasks_and_chain_import_fields_to_fyle
+from fyle_accounting_mappings.models import MappingSetting
+
+def test_construct_tasks_and_chain_import_fields_to_fyle(db):
+    workspace_id = 3
+    MappingSetting.objects.filter(workspace_id=workspace_id).delete()
+    MappingSetting.objects.create(
+        source_field='PROJECT',
+        destination_field='CUSTOMER',
+        workspace_id=workspace_id,
+        import_to_fyle=True,
+        is_custom=False
+    )
+
+    construct_tasks_and_chain_import_fields_to_fyle(workspace_id)
