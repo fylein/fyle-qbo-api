fb9d62e548935ea273ec10cc7b5b7870e30212be
Ashwin Thanaraj <37061471+ashwin1111@users.noreply.github.com>
2023-12-19 20:44:05 +0530

    Handle onboarding phase token expiry and add report_id to expense_groups/ response (#528)
    * Handle onboarding phase token expiry
    
    * add report_id to expense group response

--- apps/fyle/serializers.py
+++ apps/fyle/serializers.py
@@ -21,7 +21,7 @@ class ExpenseSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Expense
-        fields = ['updated_at', 'claim_number', 'employee_email', 'employee_name', 'fund_source', 'expense_number', 'payment_number', 'vendor', 'category', 'amount']
+        fields = ['updated_at', 'claim_number', 'employee_email', 'employee_name', 'fund_source', 'expense_number', 'payment_number', 'vendor', 'category', 'amount', 'report_id']
 
 
 class ExpenseGroupSerializer(serializers.ModelSerializer):
