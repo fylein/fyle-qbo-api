77146cf7861f330443f335c98f5bb2d59dde39e3
labhvam5 <labhvam.s@fyle.in>
2023-12-19 16:30:57 +0530

    resolving comments -2

--- apps/mappings/schedules.py
+++ apps/mappings/schedules.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+from typing import Dict, List
 from django_q.models import Schedule
 from apps.workspaces.models import WorkspaceGeneralSettings
 from fyle_accounting_mappings.models import MappingSetting
 
 
-def schedule_or_delete_fyle_import_tasks(workspace_general_settings: WorkspaceGeneralSettings, mapping_setting_instance: MappingSetting = None):
+def schedule_or_delete_fyle_import_tasks(workspace_general_settings: WorkspaceGeneralSettings, mapping_settings: List[Dict] = []):
     """
     Schedule or delete Fyle import tasks based on the workspace_general_settings and mapping_settings.
     :param workspace_general_settings: Workspace workspace_general_settings Instance
@@ -12,9 +13,10 @@ def schedule_or_delete_fyle_import_tasks(workspace_general_settings: WorkspaceGe
     :return: None
     """
     task_to_be_scheduled = None
-    # Check if there is a task to be scheduled
-    if mapping_setting_instance and mapping_setting_instance.import_to_fyle:
-        task_to_be_scheduled = mapping_setting_instance
+    for mapping_setting in mapping_settings:
+        if mapping_setting['import_to_fyle']:
+            task_to_be_scheduled = True
+            break
 
     if task_to_be_scheduled or workspace_general_settings.import_categories or workspace_general_settings.import_items:
         Schedule.objects.update_or_create(
