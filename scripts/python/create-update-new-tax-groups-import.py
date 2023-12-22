4653a7dc6560358c5571cf68ea2d821cd9ae70c6
labhvam5 <labhvam.s@fyle.in>
2023-12-22 16:31:15 +0530

    Migration script

---
+++ scripts/python/create-update-new-tax-groups-import.py
@@ -0,0 +1,43 @@
+from django.db import transaction
+import random
+from datetime import datetime, timedelta
+from django_q.models import Schedule
+from apps.workspaces.models import WorkspaceGeneralSettings
+
+existing_import_enabled_schedules = Schedule.objects.filter(
+    func__in=['apps.mappings.tasks.auto_create_tax_codes_mappings']
+).values('args')
+
+try:
+    with transaction.atomic():
+        for schedule in existing_import_enabled_schedules:
+            random_number = random.randint(1, 23)
+            workspace_general_settings = WorkspaceGeneralSettings.objects.get(workspace_id=schedule['args'])
+            if workspace_general_settings.import_tax_codes:
+                print('Creating schedule for workspace_id: ', schedule['args'])
+                # adding the new schedule
+                Schedule.objects.update_or_create(
+                    func='apps.mappings.queues.construct_tasks_and_chain_import_fields_to_fyle',
+                    args=schedule['args'],
+                    defaults={
+                        'schedule_type': Schedule.MINUTES,
+                        'minutes':24 * 60,
+                        'next_run':datetime.now() + timedelta(hours=random_number)
+                    }
+                )
+                # deleting the old schedule
+                Schedule.objects.filter(
+                    func='apps.mappings.tasks.auto_create_tax_codes_mappings',
+                    args=schedule['args']
+                ).delete()
+        # remove this sanity check after running this script
+        raise Exception("This is a sanity check")
+except Exception as e:
+    print(e)
+
+
+# Run this in sql
+# select * from django_q_schedule where func = 'apps.mappings.tasks.auto_create_tax_codes_mappings';
+# --rows should be 0
+# If not check the workspace_id and delete the row
+# delete from django_q_schedule where func = 'apps.mappings.tasks.auto_create_tax_codes_mappings' and args = 'workspace_id';
