72c3e29efb95369b565f789dbceb91222b2a864a
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2024-02-16 13:36:24 +0530

    Advance search for QBO (#563)
    * Advance search for QBO
    
    * advanced search bug fix
    
    * test case for advanced search
    
    * removed unused code
    
    * comment resolved
    
    * remove flake errors

--- apps/fyle/helpers.py
+++ apps/fyle/helpers.py
@@ -4,12 +4,15 @@ import logging
 from typing import List, Union
 
 import requests
+
+import django_filters
 from django.conf import settings
 from django.db.models import Q
 
-from apps.fyle.models import ExpenseFilter, ExpenseGroupSettings, Expense
+from apps.fyle.models import ExpenseFilter, ExpenseGroup, ExpenseGroupSettings, Expense
 from apps.tasks.models import TaskLog
 from apps.workspaces.models import WorkspaceGeneralSettings
+
 
 logger = logging.getLogger(__name__)
 logger.level = logging.INFO
@@ -245,3 +248,62 @@ def get_batched_expenses(batched_payload: List[dict], workspace_id: int) -> List
     """
     expense_ids = [expense['id'] for expense in batched_payload]
     return Expense.objects.filter(expense_id__in=expense_ids, workspace_id=workspace_id)
+
+
+class AdvanceSearchFilter(django_filters.FilterSet):
+    def filter_queryset(self, queryset):
+        or_filtered_queryset = queryset.none()
+        or_filter_fields = getattr(self.Meta, 'or_fields', [])
+        or_field_present = False
+
+        for field_name in self.Meta.fields:
+            value = self.data.get(field_name)
+            if value:
+                if field_name == 'is_skipped':
+                    value = True if str(value) == 'true' else False
+                filter_instance = self.filters[field_name]
+                queryset = filter_instance.filter(queryset, value)
+
+        for field_name in or_filter_fields:
+            value = self.data.get(field_name)
+            if value:
+                or_field_present = True
+                filter_instance = self.filters[field_name]
+                field_filtered_queryset = filter_instance.filter(queryset, value)
+                or_filtered_queryset |= field_filtered_queryset
+
+        if or_field_present:
+            queryset = queryset & or_filtered_queryset
+            return queryset
+
+        return queryset
+
+
+class ExpenseGroupSearchFilter(AdvanceSearchFilter):
+    exported_at__gte = django_filters.DateTimeFilter(lookup_expr='gte', field_name='exported_at')
+    exported_at__lte = django_filters.DateTimeFilter(lookup_expr='lte', field_name='exported_at')
+    tasklog__status = django_filters.CharFilter()
+    expenses__expense_number = django_filters.CharFilter(field_name='expenses__expense_number', lookup_expr='icontains')
+    expenses__employee_name = django_filters.CharFilter(field_name='expenses__employee_name', lookup_expr='icontains')
+    expenses__employee_email = django_filters.CharFilter(field_name='expenses__employee_email', lookup_expr='icontains')
+    expenses__claim_number = django_filters.CharFilter(field_name='expenses__claim_number', lookup_expr='icontains')
+
+    class Meta:
+        model = ExpenseGroup
+        fields = ['exported_at__gte', 'exported_at__lte', 'tasklog__status']
+        or_fields = ['expenses__expense_number', 'expenses__employee_name', 'expenses__employee_email', 'expenses__claim_number']
+
+
+class ExpenseSearchFilter(AdvanceSearchFilter):
+    org_id = django_filters.CharFilter()
+    is_skipped = django_filters.BooleanFilter()
+    updated_at = django_filters.DateTimeFromToRangeFilter()
+    expense_number = django_filters.CharFilter(field_name='expense_number', lookup_expr='icontains')
+    employee_name = django_filters.CharFilter(field_name='employee_name', lookup_expr='icontains')
+    employee_email = django_filters.CharFilter(field_name='employee_email', lookup_expr='icontains')
+    claim_number = django_filters.CharFilter(field_name='claim_number', lookup_expr='icontains')
+
+    class Meta:
+        model = Expense
+        fields = ['org_id', 'is_skipped', 'updated_at']
+        or_fields = ['expense_number', 'employee_name', 'employee_email', 'claim_number']
