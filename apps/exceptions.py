ca2365c3dffeda9ee3024e006efae1c7e110ddd4
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2024-03-28 18:54:22 +0530

    handle no privilege error (#466)
    * handle no privilege error
    
    * handle no privilege error

--- apps/exceptions.py
+++ apps/exceptions.py
@@ -1,6 +1,7 @@
 import logging
 
 from fyle.platform.exceptions import InvalidTokenError as FyleInvalidTokenError
+from fyle.platform.exceptions import NoPrivilegeError
 from qbosdk.exceptions import InvalidTokenError, WrongParamsError
 from rest_framework.response import Response
 from rest_framework.views import status
@@ -34,6 +35,10 @@ def handle_view_exceptions():
             except WrongParamsError as exception:
                 logger.info('QBO token expired workspace_id - %s %s', kwargs['workspace_id'], {'error': exception.response})
                 return Response(data={'message': 'QBO token expired workspace_id'}, status=status.HTTP_400_BAD_REQUEST)
+
+            except NoPrivilegeError as exception:
+                logger.info('Invalid Fyle Credentials / Admin is disabled  for workspace_id%s %s', kwargs['workspace_id'], {'error': exception.response})
+                return Response(data={'message': 'Invalid Fyle Credentials / Admin is disabled'}, status=status.HTTP_400_BAD_REQUEST)
 
             except InvalidTokenError as exception:
                 logger.info('QBO token expired workspace_id - %s %s', kwargs['workspace_id'], {'error': exception.response})
