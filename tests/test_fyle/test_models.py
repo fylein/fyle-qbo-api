73325bc883aa8fa6340d26bec07a96494a20a4c6
anishfyle <anishkumar.s@fyle.in>
2024-06-21 15:06:44 +0530

    revert for original test

--- tests/test_fyle/test_models.py
+++ tests/test_fyle/test_models.py
@@ -283,22 +283,23 @@ def test_create_expense_group_report_id_debit_card_expense(db):
 
 def test_create_expense_groups_by_report_id_fund_source(db):
     workspace_id = 4
-    payload = data['ccc_expenses_split']
-
-    expense_group_settings = ExpenseGroupSettings.objects.get(workspace_id=workspace_id)
-    expense_group_settings.ccc_export_date_type = 'last_spent_at'
-    expense_group_settings.save()
+    payload = data['expenses']
     Expense.create_expense_objects(payload, workspace_id)
     expense_objects = Expense.objects.last()
 
+    expense_group_settings = ExpenseGroupSettings.objects.get(workspace_id=workspace_id)
+    expense_group_settings.reimbursable_export_date_type = 'last_spent_at'
+    expense_group_settings.ccc_export_date_type = 'last_spent_at'
+    expense_group_settings.save()
+
     field = ExpenseAttribute.objects.filter(workspace_id=workspace_id, attribute_type='PROJECT').last()
-    field.attribute_type = 'ANISH'
+    field.attribute_type = 'KILLUA'
     field.save()
 
     expenses = Expense.objects.filter(id=33).all()
 
-    expense_groups = _group_expenses(expenses, ['claim_number', 'fund_source', 'project', 'employee_email', 'report_id', 'Anish'], 4)
-    assert expense_groups == [{'claim_number': 'C/2022/05/R/9', 'fund_source': 'CCC', 'project': 'Anish Sinh', 'employee_email': 'anish@fyle.in', 'report_id': 'rpawE81idoYo', 'anish': '', 'total': 1, 'expense_ids': [33]}]
+    expense_groups = _group_expenses(expenses, ['claim_number', 'fund_source', 'project', 'employee_email', 'report_id', 'Killua'], 4)
+    assert expense_groups == [{'claim_number': 'C/2022/05/R/6', 'fund_source': 'PERSONAL', 'project': 'Bebe Rexha', 'employee_email': 'sravan.kumar@fyle.in', 'report_id': 'rpawE81idoYo', 'killua': '', 'total': 1, 'expense_ids': [33]}]
 
     expense_groups = ExpenseGroup.create_expense_groups_by_report_id_fund_source([expense_objects], workspace_id)
     assert len(expense_groups) == 1
@@ -307,7 +308,7 @@ def test_create_expense_groups_by_report_id_fund_source(db):
     assert expense_groups.exported_at == None
 
     general_settings = WorkspaceGeneralSettings.objects.get(workspace_id=workspace_id)
-    general_settings.corporate_credit_card_expenses_object = 'CREDIT CARD PURCHASE'
+    general_settings.reimbursable_expenses_object = 'BILL'
     general_settings.save()
 
     expenses = expense_groups.expenses.all()
