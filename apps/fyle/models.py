b63b0c46e68be58a56b7f36f8b4128e63309ecd4
Anish Kr Singh <116036738+anishfyle@users.noreply.github.com>
2024-06-28 10:56:46 +0530

    Split Expense: Grouping function (#638)
    * Initial Commit
    
    * grouping function
    
    * model changes final
    
    * extend the filtered_corporate_credit_card_expense_groups
    
    * flake8
    
    * flake8
    
    * flake8
    
    * removed trailing spaces
    
    * pr comment

--- apps/fyle/models.py
+++ apps/fyle/models.py
@@ -432,11 +432,42 @@ class ExpenseGroup(models.Model):
             filter(lambda expense: expense.fund_source == "CCC", expense_objects)
         )
 
-        filtered_corporate_credit_card_expense_groups = _group_expenses(
-            corporate_credit_card_expenses,
-            corporate_credit_card_expense_group_field,
-            workspace_id,
-        )
+        if (
+            general_settings.corporate_credit_card_expenses_object == 'CREDIT CARD PURCHASE' and
+            expense_group_settings.split_expense_grouping == 'MULTIPLE_LINE_ITEM'
+        ):
+            ccc_expenses_without_bank_transaction = [
+                expense for expense in expense_objects
+                if expense.bank_transaction_id is None
+            ]
+            filtered_corporate_credit_card_expense_groups = _group_expenses(
+                ccc_expenses_without_bank_transaction,
+                corporate_credit_card_expense_group_field,
+                workspace_id,
+            )
+
+            ccc_expenses_with_bank_transaction = [
+                expense for expense in expense_objects
+                if expense.bank_transaction_id
+            ]
+            corporate_credit_card_expense_group_field = [
+                field for field in corporate_credit_card_expense_group_field
+                if field not in {'expense_number', 'expense_id'}
+            ]
+            corporate_credit_card_expense_group_field.append('bank_transaction_id')
+            filtered_corporate_credit_card_expense_groups.extend(
+                _group_expenses(
+                    ccc_expenses_with_bank_transaction,
+                    corporate_credit_card_expense_group_field,
+                    workspace_id,
+                )
+            )
+        else:
+            filtered_corporate_credit_card_expense_groups = _group_expenses(
+                corporate_credit_card_expenses,
+                corporate_credit_card_expense_group_field,
+                workspace_id,
+            )
 
         if (
             general_settings.corporate_credit_card_expenses_object == "BILL"
