62d8943f950572c8bc9ab9947258f083ccc31374
Hrishabh Tiwari <hrishabh.t@fyle.in>
2024-08-02 14:55:25 +0530

    fix breaking test_cases

--- tests/test_workspaces/test_apis/test_import_settings/fixtures.py
+++ tests/test_workspaces/test_apis/test_import_settings/fixtures.py
@@ -1,6 +1,6 @@
 data = {
     'import_settings': {
-        'workspace_general_settings': {'import_categories': True, 'import_items': True, 'charts_of_accounts': ['Expense', 'Cost of Goods Sold'], 'import_tax_codes': True, 'import_vendors_as_merchants': True},
+        'workspace_general_settings': {'import_categories': True, 'import_items': True, 'charts_of_accounts': ['Expense', 'Cost of Goods Sold'], 'import_tax_codes': True, 'import_vendors_as_merchants': True, 'import_code_fields': []},
         'general_mappings': {'default_tax_code': {'name': '12.5% TR @12.5%', 'id': '22'}},
         'mapping_settings': [
             {'source_field': 'COST_CENTER', 'destination_field': 'DEPARTMENT', 'import_to_fyle': True, 'is_custom': False, 'source_placeholder': 'cost center'},
@@ -9,12 +9,12 @@ data = {
         ],
     },
     'import_settings_without_mapping': {
-        'workspace_general_settings': {'import_categories': True, 'import_items': True, 'charts_of_accounts': ['Expense'], 'import_tax_codes': True, 'import_vendors_as_merchants': True},
+        'workspace_general_settings': {'import_categories': True, 'import_items': True, 'charts_of_accounts': ['Expense'], 'import_tax_codes': True, 'import_vendors_as_merchants': True, 'import_code_fields': []},
         'general_mappings': {'default_tax_code': {'name': '12.5% TR @12.5%', 'id': '22'}},
         'mapping_settings': [{'source_field': 'CLASS', 'destination_field': 'CUSTOMER', 'import_to_fyle': True, 'is_custom': True, 'source_placeholder': 'class'}],
     },
     'response': {
-        'workspace_general_settings': {'import_categories': True, 'import_items': True, 'charts_of_accounts': ['Expense'], 'import_tax_codes': True, 'import_vendors_as_merchants': True},
+        'workspace_general_settings': {'import_categories': True, 'import_items': True, 'charts_of_accounts': ['Expense'], 'import_tax_codes': True, 'import_vendors_as_merchants': True, 'import_code_fields': []},
         'general_mappings': {'default_tax_code': {'name': '12.5% TR @12.5%', 'id': '22'}},
         'mapping_settings': [
             {'source_field': 'COST_CENTER', 'destination_field': 'CLASS', 'import_to_fyle': True, 'is_custom': False, 'source_placeholder': ''},
