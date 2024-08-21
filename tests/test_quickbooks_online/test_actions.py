349e2f76207fe2e206eda82fc7a1a2d40343ddbb
Viswas Haridas <37623357+JustARatherRidiculouslyLongUsername@users.noreply.github.com>
2024-08-21 11:55:04 +0530

    fix: improve response times of sync/refresh dimensions requests (#661)
    * fix(apps/qbo): delegate sync dimensions tasks to workers
    
    * fix(apps/fyle): delegate sync dimensions tasks to workers
    
    * fix: respond with a 400 for invalid workspaces/creds
    
    * refactor: linting
    
    * test: add unit tests for functions that have been made async

--- tests/test_quickbooks_online/test_actions.py
+++ tests/test_quickbooks_online/test_actions.py
@@ -1,5 +1,7 @@
+from unittest import mock
 from apps.fyle.models import ExpenseGroup
-from apps.quickbooks_online.actions import generate_export_url_and_update_expense
+from apps.quickbooks_online.actions import generate_export_url_and_update_expense, refresh_quickbooks_dimensions, sync_quickbooks_dimensions
+from apps.workspaces.models import Workspace
 
 
 def test_generate_export_url_and_update_expense(db):
@@ -14,3 +16,29 @@ def test_generate_export_url_and_update_expense(db):
         assert expense.accounting_export_summary['error_type'] == None
         assert expense.accounting_export_summary['id'] == expense.expense_id
         assert expense.accounting_export_summary['state'] == 'COMPLETE'
+
+
+def test_refresh_quickbooks_dimensions(db):
+    workspace_id = 1
+    workspace = Workspace.objects.get(id=3)
+
+    with mock.patch('apps.quickbooks_online.utils.QBOConnector.sync_dimensions') as mock_call:
+        mock_call.return_value = None
+
+        refresh_quickbooks_dimensions(workspace_id)
+        assert workspace.destination_synced_at is not None
+
+
+def test_sync_quickbooks_dimensions(db):
+    workspace = Workspace.objects.get(id=3)
+
+    with mock.patch('apps.quickbooks_online.utils.QBOConnector.sync_dimensions') as mock_call:
+        mock_call.return_value = None
+
+        sync_quickbooks_dimensions(3)
+        assert workspace.destination_synced_at is not None
+
+        # If dimensions were synced ≤ 1 day ago, they shouldn't be synced again
+        old_destination_synced_at = workspace.destination_synced_at
+        sync_quickbooks_dimensions(3)
+        assert workspace.destination_synced_at == old_destination_synced_at
