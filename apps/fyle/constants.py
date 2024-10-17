3759068ce3d4494b5f51e5847fa8055321b8adb7
Viswas <viswas.h@fyle.in>
2024-10-17 15:29:24 +0530

    fix: change the `report_title` option to `report_name`

--- apps/fyle/constants.py
+++ apps/fyle/constants.py
@@ -1,7 +1,7 @@
 DEFAULT_FYLE_CONDITIONS = [
     {'field_name': 'employee_email', 'type': 'SELECT', 'is_custom': False},
     {'field_name': 'claim_number', 'type': 'TEXT', 'is_custom': False},
-    {'field_name': 'report_title', 'type': 'TEXT', 'is_custom': False},
+    {'field_name': 'report_name', 'type': 'TEXT', 'is_custom': False},
     {'field_name': 'spent_at', 'type': 'DATE', 'is_custom': False},
     {'field_name': 'category', 'type': 'SELECT', 'is_custom': False},
 ]
