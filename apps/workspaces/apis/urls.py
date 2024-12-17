b21a63837c8cfefcb5e0869c55acbf5bc5bf14d4
anishfyle <anishkumar.s@fyle.in>
2024-12-17 15:09:55 +0530

    fix: remove employee settings apis

--- apps/workspaces/apis/urls.py
+++ apps/workspaces/apis/urls.py
@@ -19,12 +19,10 @@ from apps.workspaces.apis.advanced_configurations.views import AdvancedConfigura
 from apps.workspaces.apis.errors.views import ErrorsView
 from apps.workspaces.apis.export_settings.views import ExportSettingsView
 from apps.workspaces.apis.import_settings.views import ImportSettingsView, ImportCodeFieldView
-from apps.workspaces.apis.map_employees.views import MapEmployeesView
 from apps.workspaces.apis.clone_settings.views import CloneSettingsView
 
 urlpatterns = [
     path('<int:workspace_id>/export_settings/', ExportSettingsView.as_view()),
-    path('<int:workspace_id>/map_employees/', MapEmployeesView.as_view()),
     path('<int:workspace_id>/import_settings/import_code_fields_config/', ImportCodeFieldView.as_view(), name='import-code-fields-config'),
     path('<int:workspace_id>/import_settings/', ImportSettingsView.as_view()),
     path('<int:workspace_id>/advanced_configurations/', AdvancedConfigurationsView.as_view()),
