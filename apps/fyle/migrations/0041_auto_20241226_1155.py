01f88a3b4d4d1a0e9d491126325e05568d39748e
Ashutosh619-sudo <ashutoshsinghrkt@gmail.com>
2025-01-07 17:20:41 +0530

    Feat: Resolve migration issue

--- apps/fyle/migrations/0041_auto_20241226_1155.py
+++ apps/fyle/migrations/0041_auto_20241226_1155.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('fyle', '0040_expense_masked_corporate_card_number'),
+        ('tasks', '0012_auto_20241127_0730'),
     ]
 
     operations = [
