402c262562ca9598c6c39a5857a2550f5c460128
anishfyle <anishkumar.s@fyle.in>
2025-01-31 10:22:15 +0530

    removed unused import

--- apps/fyle/signals.py
+++ apps/fyle/signals.py
@@ -3,13 +3,9 @@ Fyle Signals
 """
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from fyle_accounting_mappings.models import DestinationAttribute, EmployeeMapping
 from pydantic import ValidationError
 
 from apps.fyle.models import ExpenseFilter
-from apps.quickbooks_online.queue import async_run_post_configration_triggers
-from apps.workspaces.models import Workspace, WorkspaceGeneralSettings
-from apps.workspaces.utils import delete_cards_mapping_settings
 
 
 @receiver(post_save, sender=ExpenseFilter)
