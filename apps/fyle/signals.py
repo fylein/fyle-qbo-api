a69c16315d7298689eb1c1cb24822421d23efe5d
anishfyle <anishkumar.s@fyle.in>
2025-02-04 17:18:41 +0530

    fix validation error

--- apps/fyle/signals.py
+++ apps/fyle/signals.py
@@ -24,4 +24,4 @@ def run_post_save_expense_filters(sender, instance: ExpenseFilter, **kwargs):
             re_run_skip_export_rule(instance.workspace_id, None)
         except Exception as e:
             logger.error(f'Error while processing expense filter for workspace_id: {instance.workspace_id} - {str(e)}')
-            raise ValidationError
+            raise ValidationError(f'Failed to process expense filter: {str(e)}') from e
