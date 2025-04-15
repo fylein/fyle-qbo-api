e4dbe9b8286ce88c9969fc61a44abd63d1244f46
ruuushhh <rushikeshtayade2001@gmail.com>
2025-04-15 23:54:52 +0530

    added fix

--- apps/workspaces/actions.py
+++ apps/workspaces/actions.py
@@ -261,11 +261,12 @@ def export_to_qbo(workspace_id, export_mode=None, expense_group_ids=[], is_direc
     if not active_qbo_credentials:
         if is_direct_export:
             failed_expense_ids = []
-            for expense_group_id in expense_group_ids:
+            for index, expense_group_id in enumerate(expense_group_ids):
                 expense_group = ExpenseGroup.objects.get(id=expense_group_id)
-                first_expense = expense_group.expenses.first()
-                update_expenses_in_progress([first_expense])
-                post_accounting_export_summary(workspace_id=workspace_id, expense_ids=[first_expense.id])
+                if index == 0:
+                    first_expense = expense_group.expenses.first()
+                    update_expenses_in_progress([first_expense])
+                    post_accounting_export_summary(workspace_id=workspace_id, expense_ids=[first_expense.id])
                 update_failed_expenses(expense_group.expenses.all(), False, True)
                 failed_expense_ids.extend(expense_group.expenses.values_list('id', flat=True))
             post_accounting_export_summary(workspace_id=workspace_id, expense_ids=failed_expense_ids, is_failed=True)
