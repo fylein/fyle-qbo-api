5b462f13fd04158f60ed6d78af636cde7f07d83a
Ashutosh singh <55102089+Ashutosh619-sudo@users.noreply.github.com>
2025-06-12 21:12:01 +0530

    Feat: Bug fix where Credit card account type selected for debit card expense (#827)
    * Feat: Bug fix where Credit card account type selected for debit card expense
    
    * resolve circular import
    
    * refactor code for removing circular import issue
    
    * add code
    
    * flake resolved
    
    * changing code for circular improt error
    
    * comment resolved
    
    * flake resolve
    
    * redsolved test
    
    ---------
    
    Co-authored-by: Ashutosh619-sudo <ashutosh.s@fyle.com>

--- apps/quickbooks_online/models.py
+++ apps/quickbooks_online/models.py
@@ -486,10 +486,11 @@ class QBOExpense(models.Model):
         db_table = 'qbo_expenses'
 
     @staticmethod
-    def create_qbo_expense(expense_group: ExpenseGroup):
+    def create_qbo_expense(expense_group: ExpenseGroup, qbo_connection):
         """
         Create QBO Expense
         :param expense_group: expense group
+        :param qbo_connection: QBO Connection object
         :return: QBO Expense object
         """
         description = expense_group.description
@@ -521,7 +522,12 @@ class QBOExpense(models.Model):
             if not entity:
                 destination_attribute = DestinationAttribute.objects.filter(destination_id=account_id, workspace_id=expense_group.workspace_id).first()
                 payee_type = 'Debit Card Misc' if destination_attribute.attribute_type == 'BANK_ACCOUNT' else 'Credit Card Misc'
-                entity_id = DestinationAttribute.objects.filter(value=payee_type, workspace_id=expense_group.workspace_id).first().destination_id
+                # In case credit card account is selected, we need to create credit card misc vendor
+                entity_id = DestinationAttribute.objects.filter(value=payee_type, workspace_id=expense_group.workspace_id).first()
+                if not entity_id:
+                    entity_id = qbo_connection.get_or_create_vendor(payee_type, create=True).destination_id
+                else:
+                    entity_id = entity_id.destination_id
             else:
                 entity_id = entity.destination_id
 
