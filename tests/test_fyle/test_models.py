0b36e9f70ef99c3809fa10e37bf44dee78c06e84
ruuushhh <66899387+ruuushhh@users.noreply.github.com>
2025-06-13 20:59:00 +0530

    fix: rounding (#824)
    * fix: rounding to 3 decimal
    
    * remove rounding

--- tests/test_fyle/test_models.py
+++ tests/test_fyle/test_models.py
@@ -9,7 +9,6 @@ from apps.fyle.models import (
     WorkspaceGeneralSettings,
     _format_date,
     _group_expenses,
-    _round_to_currency_fraction,
     get_default_ccc_expense_state,
     get_default_expense_group_fields,
     get_default_expense_state,
@@ -403,35 +402,6 @@ def test_format_date():
     date_string = _format_date('2022-05-13T09:32:06.643941Z')
 
     assert date_string == parser.parse('2022-05-13T09:32:06.643941Z')
-
-
-def test_amount_rounding():
-    amount = _round_to_currency_fraction(81.6455, 'USD')
-    assert amount == 81.65
-
-    amount = _round_to_currency_fraction(81.642, 'USD')
-    assert amount == 81.64
-
-    amount = _round_to_currency_fraction(81.6455, 'TND')
-    assert amount == 81.646
-
-    amount = _round_to_currency_fraction(81.64555, 'TND')
-    assert amount == 81.646
-
-    amount = _round_to_currency_fraction(81.64, 'JOD')
-    assert amount == 81.64
-
-    amount = _round_to_currency_fraction(86.4445, 'JOD')
-    assert amount == 86.445
-
-    amount = _round_to_currency_fraction(86.4445, 'USD')
-    assert amount == 86.44
-
-    amount = _round_to_currency_fraction(86.445, 'USD')
-    assert amount == 86.45
-
-    amount = _round_to_currency_fraction(86.444, 'USD')
-    assert amount == 86.44
 
 
 def test_create_expense_object_tax_amount(db):
