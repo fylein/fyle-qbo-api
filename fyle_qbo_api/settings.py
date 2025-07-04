3d153e024363d476ce199a5667eb3731b64aec55
Ashutosh619-sudo <ashutosh.s@fyle.com>
2025-07-04 12:56:13 +0530

    Feat: Fix throttling vulnerability

--- fyle_qbo_api/settings.py
+++ fyle_qbo_api/settings.py
@@ -97,6 +97,14 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('fyle_rest_auth.authentication.FyleJWTAuthentication',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'PAGE_SIZE': 100,
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle',
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '10/second',
+        'user': '10/second'
+    }
 }
 
 WSGI_APPLICATION = 'fyle_qbo_api.wsgi.application'
