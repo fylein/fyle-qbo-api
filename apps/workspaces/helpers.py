3a424e4c7d35404a8e6f05c29f370452dbb84d20
Hrishabh Tiwari <74908943+Hrishabh17@users.noreply.github.com>
2025-07-14 21:03:18 +0530

    fix: enable/disable multi currency support (#857)
    * fix: enable/disable multi currency support
    
    * fix cov
    
    * fix comment
    
    * fix add check for qbo creds
    
    * fix comment

--- apps/workspaces/helpers.py
+++ apps/workspaces/helpers.py
@@ -1,3 +1,12 @@
+import logging
+from datetime import datetime, timezone
+
+from apps.workspaces.models import QBOCredential, WorkspaceGeneralSettings
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
 def get_error_model_path() -> str:
     """
     Get error model path: This is for imports submodule
@@ -20,3 +29,22 @@ def get_app_name() -> str:
     :return: str
     """
     return 'QUICKBOOKS'
+
+
+def enable_multi_currency_support(workspace_general_settings: WorkspaceGeneralSettings) -> None:
+    """
+    Enable multi currency support
+    :param workspace_general_settings: WorkspaceGeneralSettings
+    :return: None
+    """
+    workspace_id = workspace_general_settings.workspace.id
+    qbo_credential = QBOCredential.objects.filter(workspace_id=workspace_id).first()
+    if not qbo_credential:
+        return
+
+    if qbo_credential.currency and qbo_credential.currency != workspace_general_settings.workspace.fyle_currency:
+        logger.info(f'Enabling multi currency support for workspace {workspace_id}')
+        WorkspaceGeneralSettings.objects.filter(workspace_id=workspace_id).update(
+            is_multi_currency_allowed=True,
+            updated_at=datetime.now(timezone.utc)
+        )
